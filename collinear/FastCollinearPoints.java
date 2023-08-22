7f97dc1c98b81c113f899bda549d05c550f67bce
your-hamster <svetozar.volskyi@gmail.com>
2023-08-22 18:35:23 +0300

    stable version

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 public class FastCollinearPoints {
     
     private ArrayList<LineSegment> segments = new ArrayList<>();
@@ -10,13 +9,11 @@ public class FastCollinearPoints {
         if (points == null) {
             throw new java.lang.IllegalArgumentException("the argument to the constructor is null");
         }
-
         for (int i = 0; i < points.length; i++) {
             if (points[i] == null) {
                 throw new java.lang.IllegalArgumentException("a point in the array is null");
             }
         }
-
         for (int i = 0; i < points.length; i++) {
             for (int j = 0; j < points.length; j++) {
                 if (points[i].compareTo(points[j]) == 0) {
@@ -25,56 +22,38 @@ public class FastCollinearPoints {
                 }
             }
         }
-
         for (int i = 0; i < points.length; i++) {
-            Point currentPoint = points[i];
             ArrayList<Point> pointsSet = new ArrayList<>();
             for (int j = 0; j < points.length; j++) {
                 if (i == j) { continue; }
                 pointsSet.add(points[j]);
             }
-            pointsSet.sort(currentPoint.slopeOrder());
+            pointsSet.sort(points[i].slopeOrder());
             
             ArrayList<Point> collinearPoints = new ArrayList<>();
+            int lastAddedPointer = -1;
             int leftPointer = 0;
-            int numOfDotsWithEqualSlopes = 0;
-            int lastCollPointPointer = -1;
             for (int rightPointer = 1; rightPointer < pointsSet.size(); rightPointer++) {
                 Point pLeft = pointsSet.get(leftPointer);
                 Point pRight = pointsSet.get(rightPointer);
-                double slopeLeft = currentPoint.slopeTo(pLeft);
-                double slopeRight = currentPoint.slopeTo(pRight);
+                double slopeLeft = points[i].slopeTo(pLeft);
+                double slopeRight = points[i].slopeTo(pRight);
                 if (slopeLeft == slopeRight) {
-                    numOfDotsWithEqualSlopes = 2;
-                    for (int j = rightPointer+1; j < pointsSet.size(); j++) {
-                        Point p = pointsSet.get(j);
-                        double slope = currentPoint.slopeTo(p);
-                        if (slope == slopeRight) {
-                            numOfDotsWithEqualSlopes++;
-                        } else {
-                            lastCollPointPointer = j-1;
-                            break;
-                        }
+                    if (lastAddedPointer != leftPointer) {
+                        collinearPoints.add(pointsSet.get(leftPointer));
                     }
-                    
-                    for (int j = leftPointer; j <= lastCollPointPointer; j++) {
-                        collinearPoints.add(pointsSet.get(j));
-                    }
-                    
-                    leftPointer = lastCollPointPointer;
-                    rightPointer = lastCollPointPointer + 1;
-
-                    collinearPoints.add(currentPoint);
-                    if (collinearPoints.size() > 4  &&  numOfDotsWithEqualSlopes > 3) {
-                        collinearPoints.sort(null);
-                        LineSegment segment = new LineSegment(collinearPoints.get(0), collinearPoints.get(collinearPoints.size() - 1));
-                        segments.add(segment);
-                        numberOfSegments++;
-                    }
-
-                } else {
-                    leftPointer++;
+                    collinearPoints.add(pointsSet.get(rightPointer));
+                    lastAddedPointer = rightPointer;
                 }
+                leftPointer++;
+            }
+
+            collinearPoints.add(points[i]);
+            if (collinearPoints.size() > 4) {
+                collinearPoints.sort(null);
+                LineSegment segment = new LineSegment(collinearPoints.get(0), collinearPoints.get(collinearPoints.size() - 1));
+                segments.add(segment);
+                numberOfSegments++;
             }
         }
     }
