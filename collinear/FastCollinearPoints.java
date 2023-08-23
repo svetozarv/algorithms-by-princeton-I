2cc016c7416e89b7acd28e395b811bb76831a1ef
your-hamster <svetozar.volskyi@gmail.com>
2023-08-23 12:16:14 +0300

    redistributed code into functions for better understanding

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -7,53 +7,39 @@ public class FastCollinearPoints {
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
         if (points == null) {
-            throw new java.lang.IllegalArgumentException("the argument to the constructor is null");
+            throw new java.lang.IllegalArgumentException("The argument to the constructor is null");
         }
         for (int i = 0; i < points.length; i++) {
             if (points[i] == null) {
-                throw new java.lang.IllegalArgumentException("a point in the array is null");
+                throw new java.lang.IllegalArgumentException("A point in the array is null");
             }
         }
         for (int i = 0; i < points.length; i++) {
             for (int j = 0; j < points.length; j++) {
                 if (points[i].compareTo(points[j]) == 0) {
                     if (i == j) { continue; }
-                    throw new java.lang.IllegalArgumentException("the argument to the constructor contains a repeated point: " + points[j]);
+                    throw new java.lang.IllegalArgumentException("The argument to the constructor contains a repeated point: " + points[j]);
                 }
             }
         }
+
+
         for (int i = 0; i < points.length; i++) {
+            Point originPoint = points[i];
             ArrayList<Point> pointsSet = new ArrayList<>();
             for (int j = 0; j < points.length; j++) {
                 if (i == j) { continue; }
                 pointsSet.add(points[j]);
             }
-            pointsSet.sort(points[i].slopeOrder());
+            pointsSet.sort(originPoint.slopeOrder());
             
             ArrayList<Point> collinearPoints = new ArrayList<>();
-            int lastAddedPointer = -1;
-            int leftPointer = 0;
-            for (int rightPointer = 1; rightPointer < pointsSet.size(); rightPointer++) {
-                Point pLeft = pointsSet.get(leftPointer);
-                Point pRight = pointsSet.get(rightPointer);
-                double slopeLeft = points[i].slopeTo(pLeft);
-                double slopeRight = points[i].slopeTo(pRight);
-                if (slopeLeft == slopeRight) {
-                    if (lastAddedPointer != leftPointer) {
-                        collinearPoints.add(pointsSet.get(leftPointer));
-                    }
-                    collinearPoints.add(pointsSet.get(rightPointer));
-                    lastAddedPointer = rightPointer;
-                }
-                leftPointer++;
-            }
+            findAdjacentCollPoints(originPoint, pointsSet, collinearPoints);
 
-            collinearPoints.add(points[i]);
-            if (collinearPoints.size() > 4) {
-                collinearPoints.sort(null);
-                LineSegment segment = new LineSegment(collinearPoints.get(0), collinearPoints.get(collinearPoints.size() - 1));
-                segments.add(segment);
-                numberOfSegments++;
+            collinearPoints.add(originPoint);
+            collinearPoints.sort(null);
+            if (collinearPoints.size() >= 4  &&  originPoint.compareTo(collinearPoints.get(0)) == 0) {
+                addLineSegment(collinearPoints.get(0), collinearPoints.get(collinearPoints.size() - 1));
             }
         }
     }
@@ -71,5 +57,36 @@ public class FastCollinearPoints {
             segmentsArray[i] = segments.get(i);
         }
         return segmentsArray;
+    }
+
+    // simply create a line segment
+    private void addLineSegment(Point a, Point b) {
+        LineSegment segment = new LineSegment(a, b);
+        segments.add(segment);
+        numberOfSegments++;
+    }
+
+    // find points with equal slopes in pointsSet and store them in collinearPoints
+    private void findAdjacentCollPoints(Point originPoint, ArrayList<Point> pointsSet, ArrayList<Point> collinearPoints) {
+        boolean foundCollPoints = false;
+        int lastAddedPointer = -1;
+        int leftPointer = 0;
+        for (int rightPointer = 1; rightPointer < pointsSet.size(); rightPointer++) {
+            Point pLeft = pointsSet.get(leftPointer);
+            Point pRight = pointsSet.get(rightPointer);
+            double slopeLeft = originPoint.slopeTo(pLeft);
+            double slopeRight = originPoint.slopeTo(pRight);
+            if (slopeLeft == slopeRight) {
+                if (lastAddedPointer != leftPointer) {
+                    collinearPoints.add(pointsSet.get(leftPointer));
+                }
+                collinearPoints.add(pointsSet.get(rightPointer));
+                lastAddedPointer = rightPointer;
+                foundCollPoints = true;
+            } else { 
+                if (foundCollPoints) { break; }
+            }
+            leftPointer++;
+        }
     }
 }
